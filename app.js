7b97ad5b59e9425d2f650317657b5822082f2747
Cedric <cedricnfp@gmail.com>
2021-07-07 22:48:05 +0400

    added tab nav

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const tabs = document.getElementsByClassName('tab');
 const tabContent = document.getElementsByClassName('tab-content');
 
-for (var i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener('click', function() {
-        document.querySelector('.active').classList.remove("active");
-        tabContent[i].className += ' active';
-    })
+
+function handleEvent(event, tab) {
+    // Loop for tab content
+    for (var i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    // Loop for tabs
+    for (var j = 0; j < tabs.length; j++) {
+        tabs[j].className = tabs[j].className.replace(" active", "");
+    }
+
+    // Display content of the current tab
+    document.getElementById(tab).style.display = "block";
+    // Add active class to current tab
+    event.currentTarget.className += " active";
 }
 
+// Active tab by default
+document.querySelector('.active').click();
