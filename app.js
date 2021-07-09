77833a926e488ea3f35345b93a774cb321f9e765
Cedric <cedricnfp@gmail.com>
2021-07-09 18:10:44 +0400

    added food menu

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const DESSERTS_API = "json/desserts.json";
+
 const tabs = document.getElementsByClassName('tab');
 const tabContent = document.getElementsByClassName('tab-content');
 
+window.onload = init();
+
+function init() {
+    loadDesserts();
+}
 
 function handleEvent(event, tab) {
     // Loop for tab content
@@ -21,3 +28,30 @@ function handleEvent(event, tab) {
 
 // Active tab by default
 document.querySelector('.active').click();
+
+
+
+function loadDesserts() {
+    fetch(DESSERTS_API)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(dessert => {
+                const card = document.createElement("div");
+                const name = document.createElement("span");
+                const cardImage = document.createElement("img");
+                
+                card.className = "cards";
+        
+                name.className = "label";
+                name.innerHTML = dessert.name;
+
+                cardImage.className = "cards-img";
+                cardImage.src = dessert.image;
+
+                card.append(name);
+                card.append(cardImage);
+                document.getElementById("desserts").appendChild(card);
+            })
+            console.table(data);
+        });  
+}
