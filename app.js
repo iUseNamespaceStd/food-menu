21497dd7a2eab4415c1ddd2d1f031e637acaa14c
Cedric <cedricnfp@gmail.com>
2021-07-10 00:44:11 +0400

    fixed card positioning

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const DESSERTS_API = "json/desserts.json";
+const MAIN_COURSES_API = "json/mainCourses.json";
+const SIDE_DISHES_API = "json/sideDishes.json";
+const STARTERS_API = "json/starters.json";
 
 const tabs = document.getElementsByClassName('tab');
 const tabContent = document.getElementsByClassName('tab-content');
 
-window.onload = init();
+init();
 
 function init() {
     loadDesserts();
+    loadMainCourses();
+    loadSideDishes();
+    loadStarters();
 }
 
 function handleEvent(event, tab) {
@@ -21,7 +27,7 @@ function handleEvent(event, tab) {
     }
 
     // Display content of the current tab
-    document.getElementById(tab).style.display = "block";
+    document.getElementById(tab).style.display = "grid";
     // Add active class to current tab
     event.currentTarget.className += " active";
 }
@@ -29,8 +35,7 @@ function handleEvent(event, tab) {
 // Active tab by default
 document.querySelector('.active').click();
 
-
-
+// Load desserts tab
 function loadDesserts() {
     fetch(DESSERTS_API)
         .then(res => res.json())
@@ -38,6 +43,8 @@ function loadDesserts() {
             data.forEach(dessert => {
                 const card = document.createElement("div");
                 const name = document.createElement("span");
+                const wrapper = document.createElement("div");
+                const ratings = document.createElement("span");;
                 const cardImage = document.createElement("img");
                 
                 card.className = "cards";
@@ -45,12 +52,152 @@ function loadDesserts() {
                 name.className = "label";
                 name.innerHTML = dessert.name;
 
+                wrapper.className = "wrapper";
+
                 cardImage.className = "cards-img";
                 cardImage.src = dessert.image;
 
+                if (dessert.ratings > 0 && dessert.ratings <= 5) {
+                    for (let i = 0; i < dessert.ratings; i++) {
+                        ratings.innerHTML += '<i class="fas fa-star"></i>'
+                    }
+                } else {
+                    for (let j = 0; j < 5; j++) {
+                        ratings.innerHTML += '<i class="far fa-star"></i>';
+                    }
+                }
+
                 card.append(name);
+                card.append(wrapper);
+                wrapper.append(ratings);
                 card.append(cardImage);
                 document.getElementById("desserts").appendChild(card);
+            })
+            console.table(data);
+        });  
+}
+
+// Load main courses
+function loadMainCourses() {
+    fetch(MAIN_COURSES_API)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(dessert => {
+                const card = document.createElement("div");
+                const name = document.createElement("span")
+                const wrapper = document.createElement("div");
+                const ratings = document.createElement("span");;
+                const cardImage = document.createElement("img");
+                
+                card.className = "cards";
+        
+                name.className = "label";
+                name.innerHTML = dessert.name;
+
+                wrapper.className = "wrapper";
+
+                cardImage.className = "cards-img";
+                cardImage.src = dessert.image;
+
+                if (dessert.ratings > 0 && dessert.ratings <= 5) {
+                    for (let i = 0; i < dessert.ratings; i++) {
+                        ratings.innerHTML += '<i class="fas fa-star"></i>'
+                    }
+                } else {
+                    for (let j = 0; j < 5; j++) {
+                        ratings.innerHTML += '<i class="far fa-star"></i>';
+                    }
+                }
+
+                card.append(name);
+                card.append(wrapper);
+                wrapper.append(ratings);
+                card.append(cardImage);
+                document.getElementById("main-course").appendChild(card);
+            })
+            console.table(data);
+        });  
+}
+
+// Load side dishes
+function loadSideDishes() {
+    fetch(SIDE_DISHES_API)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(dessert => {
+                const card = document.createElement("div");
+                const name = document.createElement("span");
+                const wrapper = document.createElement("div");
+                const ratings = document.createElement("span");
+                const cardImage = document.createElement("img");
+                
+                card.className = "cards";
+        
+                name.className = "label";
+                name.innerHTML = dessert.name;
+                
+                wrapper.className = "wrapper";
+
+                cardImage.className = "cards-img";
+                cardImage.src = dessert.image;
+
+                if (dessert.ratings > 0 && dessert.ratings <= 5) {
+                    for (let i = 0; i < dessert.ratings; i++) {
+                        ratings.innerHTML += '<i class="fas fa-star"></i>'
+                    }
+                } else {
+                    for (let j = 0; j < 5; j++) {
+                        ratings.innerHTML += '<i class="far fa-star"></i>';
+                    }
+                }
+
+                card.append(name);
+                card.append(wrapper);
+                wrapper.append(ratings);
+                card.append(cardImage);
+                document.getElementById("side-dishes").appendChild(card);
+            })
+            console.table(data);
+        });  
+}
+
+// Load starters
+function loadStarters() {
+    fetch(STARTERS_API)
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(dessert => {
+                const card = document.createElement("div");
+                const name = document.createElement("span");
+                const wrapper = document.createElement("div");
+                const ratings = document.createElement("span");
+                const cardImage = document.createElement("img");
+                
+                card.className = "cards";
+        
+                name.className = "label";
+                name.innerHTML = dessert.name;
+
+                wrapper.className = "wrapper";
+
+                cardImage.className = "cards-img";
+                cardImage.src = dessert.image;
+
+                if (dessert.ratings > 0 && dessert.ratings <= 5) {
+                    for (let i = 0; i < dessert.ratings; i++) {
+                        ratings.innerHTML += '<i class="fas fa-star"></i>'
+                    }
+                } else {
+                    for (let j = 0; j < 5; j++) {
+                        ratings.innerHTML += '<i class="far fa-star"></i>';
+                    }
+                }
+
+                card.append(name);
+                card.append(wrapper);
+                wrapper.append(ratings);
+                card.append(cardImage);
+                document.getElementById("starters").appendChild(card);
             })
             console.table(data);
         });  
